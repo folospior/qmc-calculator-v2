590b76a09eb4e57386c7cd9c6ebc9fedcda4ba73
folospior <folosp2@gmail.com>
2024-11-12 19:38:51 +0100

    add nix :fire:

--- flake.nix
+++ flake.nix
@@ -6,17 +6,18 @@
 
     outputs = { self, nixpkgs, ... }:
         let
-            pkgs = nixpkgs.legacyPackages.${builtins.currentSystem};
+            pkgs = nixpkgs.legacyPackages."x86_64-linux";
         in
         {
-            devShell = pkgs.mkShell {
+            devShells."x86_64-linux".default = pkgs.mkShell {
                 buildInputs = with pkgs; [ go gopls git direnv gofumpt ];
 
-		GO111MODULE = "on";
-		GOROOT = pkgs.go;
-		GOPATH = "${pkgs.go}/go";
-		PATH = "${pkgs.go}/bin:${pkgs.gopls}/bin:${pkgs.gofumpt}/bin";
-	    };
+		        GO111MODULE = "on";
+		        GOROOT = pkgs.go;
+		        GOPATH = "${pkgs.go}/go";
+		        #PATH = "${pkgs.go}/bin:${pkgs.gopls}/bin:${pkgs.gofumpt}/bin";
+	        };
         };
+
 	
 }
